6a534ae56716315ba675ff44153724d1f85c710b
ley <adrianmartinzgz@hotmail.com>
2024-03-16 17:28:29 +0100

    UnixMC Laboratory barrel tp

--- src/main/java/me/ley/leythings/LeyThings.java
+++ src/main/java/me/ley/leythings/LeyThings.java
@@ -2,7 +2,6 @@ package me.ley.leythings;
 
 import org.bukkit.*;
 import org.bukkit.entity.Player;
-import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -15,9 +14,8 @@ import java.util.Set;
 
 public final class LeyThings extends JavaPlugin implements Listener {
 
-    private Location teleportLocation = new Location(Bukkit.getWorld("spawn"), -46, 23, -37);
-    private boolean teleporting = false;
     private Set<Player> cooldownPlayers = new HashSet<>();
+    private Set<Player> teleportingPlayers = new HashSet<>();
 
     @Override
     public void onEnable() {
@@ -29,23 +27,25 @@ public final class LeyThings extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        if (!cooldownPlayers.contains(player) && event.getAction() == Action.RIGHT_CLICK_BLOCK && !teleporting) {
+        if (!cooldownPlayers.contains(player) && event.getAction() == Action.RIGHT_CLICK_BLOCK && !teleportingPlayers.contains(player)) {
             if (event.getClickedBlock().getType() == Material.BARREL) {
                 Location barrelLocation = event.getClickedBlock().getLocation();
                 World world = barrelLocation.getWorld();
-                // Verificar si el barril está en las coordenadas específicas
                 if (world != null && world.getName().equals("spawn")) {
                     if ((barrelLocation.getBlockX() == -49 && barrelLocation.getBlockY() == 9 && barrelLocation.getBlockZ() == -53)
                             || (barrelLocation.getBlockX() == -45 && barrelLocation.getBlockY() == 24 && barrelLocation.getBlockZ() == -37)) {
-                        // Verificar si el jugador está en modo survival
-                        if (player.getGameMode() == GameMode.SURVIVAL) {
-                            activateTeleportSequence(player, barrelLocation);
-                            event.setCancelled(true); // Cancelar el evento de interactuar con el barril
-                            teleporting = true;
-                            cooldownPlayers.add(player);
-                            Bukkit.getScheduler().runTaskLater(this, () -> cooldownPlayers.remove(player), 156);
+                        if (player.hasPermission("lab_barrel_tp")) {
+                            if (player.getGameMode() == GameMode.SURVIVAL) {
+                                teleportingPlayers.add(player);
+                                activateTeleportSequence(player, barrelLocation);
+                                event.setCancelled(true);
+                                cooldownPlayers.add(player);
+                                Bukkit.getScheduler().runTaskLater(this, () -> cooldownPlayers.remove(player), 156);
+                            } else {
+                                player.sendMessage(ChatColor.RED + "You must be in survival mode to use this.");
+                            }
                         } else {
-                            player.sendMessage("You must be in survival mode");
+                            player.sendMessage(ChatColor.RED + "You do not have permission to use this.");
                         }
                     }
                 }
@@ -58,20 +58,16 @@ public final class LeyThings extends JavaPlugin implements Listener {
             @Override
             public void run() {
                 player.performCommand("screeneffect fullscreen BLACK 5 40 5 freeze " + player.getName());
-                // Esperar 0.2 segundos
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        // Sonido del barril abriendo
                         player.playSound(player.getLocation(), Sound.BLOCK_BARREL_OPEN, 1, 1);
                     }
                 }.runTaskLater(LeyThings.this, 4);
 
-                // Sonidos y teletransporte
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        // Sonido de las hojas de cerezo rompiéndose (primer sonido)
                         player.playSound(player.getLocation(), Sound.BLOCK_CHERRY_LEAVES_BREAK, 1, 0.1f);
                     }
                 }.runTaskLater(LeyThings.this, 14);
@@ -79,7 +75,6 @@ public final class LeyThings extends JavaPlugin implements Listener {
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        // Sonido de las hojas de cerezo rompiéndose (segundo sonido)
                         player.playSound(player.getLocation(), Sound.BLOCK_CHERRY_LEAVES_BREAK, 1, 0.1f);
                     }
                 }.runTaskLater(LeyThings.this, 20);
@@ -87,7 +82,6 @@ public final class LeyThings extends JavaPlugin implements Listener {
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        // Sonido de las hojas de cerezo rompiéndose (tercer sonido)
                         player.playSound(player.getLocation(), Sound.BLOCK_CHERRY_LEAVES_BREAK, 1, 0.1f);
                     }
                 }.runTaskLater(LeyThings.this, 26);
@@ -95,7 +89,6 @@ public final class LeyThings extends JavaPlugin implements Listener {
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        // Teletransportar al jugador
                         teleportPlayer(player, barrelLocation);
                     }
                 }.runTaskLater(LeyThings.this, 46);
@@ -103,8 +96,8 @@ public final class LeyThings extends JavaPlugin implements Listener {
                 new BukkitRunnable() {
                     @Override
                     public void run() {
-                        // Sonido del barril cerrándose
                         player.playSound(player.getLocation(), Sound.BLOCK_BARREL_CLOSE, 1, 1);
+                        teleportingPlayers.remove(player);
                     }
                 }.runTaskLater(LeyThings.this, 56);
             }
@@ -114,17 +107,13 @@ public final class LeyThings extends JavaPlugin implements Listener {
     private void teleportPlayer(Player player, Location barrelLocation) {
         World world = player.getWorld();
         Location teleportLocation;
-        // Definir las coordenadas de teletransporte dependiendo del barril interactuado
         if (barrelLocation.getBlockX() == -49 && barrelLocation.getBlockY() == 9 && barrelLocation.getBlockZ() == -53) {
             teleportLocation = new Location(world, -45.5, 23, -36.6, 80, 0);
         } else {
             teleportLocation = new Location(world, -47.5, 8, -52.5, -90, 0);
         }
         player.teleport(teleportLocation);
-        teleporting = false;
     }
-
-
 
     @Override
     public void onDisable() {
